297c86868c213050f5579665dad0fc83cb05a1ba
Lynn000 <lynn000123@hotmail.com>
2016-07-04 11:48:23 +0800

    创建弹窗按钮视图
    创建弹窗按钮视图工程

---
+++ LNPopButtonView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LNPopButtonView
+//
+//  Created by David on 16/7/4.
+//  Copyright © 2016年 topcoders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ LNPopButtonView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  LNPopButtonView
+//
+//  Created by David on 16/7/4.
+//  Copyright © 2016年 topcoders. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
